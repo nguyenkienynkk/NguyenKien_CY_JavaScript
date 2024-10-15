eac7dcd4fec8c065c8d2d38c3a68a2289af50f9e
nguyenkien <nguyenkienynk@gmail.com>
2024-10-15 14:25:01 +0700

    push 7,8,9,10

--- baitap.js
+++ baitap.js
@@ -55,7 +55,7 @@ const soNguyenTo = (number) => {
             return number + " không phải là số nguyên tố";
     return number + " là số nguyên tố";
 }
-console.log(perfect(28));
+console.log(perfect(28))
 console.log(soNguyenTo(4))
 //bai4
 const validateEmail = (email) => {
@@ -91,7 +91,6 @@ const averageNumbersInString = (str) => {
 };
 console.log(averageNumbersInString(" Xin 20 c{h}ào 60  #Cy30@!Tech(VN)  100 "));
 //Cau 6
-
 const changeJson = (arrays) => {
     const resultx = arrays.reduce((acc, item) => {
         const existingGroup = acc.find(group => group[0].brand === item.brand);
@@ -106,11 +105,54 @@ const changeJson = (arrays) => {
 }
 
 const rray = [
-    { brand: 'Huyndai', model: 'Santafe' },
-    { brand: 'Huyndai', model: 'Sonata' },
-    { brand: 'Vinfast', model: 'Lux SA' },
-    { brand: 'Toyota', model: 'Carmy' },
-    { brand: 'Toyota', model: 'Lux A' },
-    { brand: 'Toyota', model: 'Vios' }
+    {brand: 'Huyndai', model: 'Santafe'},
+    {brand: 'Huyndai', model: 'Sonata'},
+    {brand: 'Vinfast', model: 'Lux SA'},
+    {brand: 'Toyota', model: 'Carmy'},
+    {brand: 'Toyota', model: 'Lux A'},
+    {brand: 'Toyota', model: 'Vios'}
 ];
 console.log(changeJson(rray));
+//Cau 7
+const nestedArray = [1, [2, [3, [4, 5,6,7,8,9]]]];
+console.log(nestedArray.flat().flat().flat());
+//Cau 8
+const userDetails = {
+    name: "John Doe",
+    age: 14,
+    verified: {
+        status: true,
+        date: new Date()
+    }
+};
+const clone = {...userDetails};
+console.log(clone);
+
+//Cau 9
+const matrix = [
+    [1,2,3],
+    [4,5,6],
+    [9,10,11]
+]
+for (let col = 0; col < matrix[0].length; col++) {
+    let columnValues = [];
+    for (let row = matrix.length - 1; row >= 0; row--) {
+        columnValues.push(matrix[row][col]);
+    }
+    console.log(columnValues);
+}
+// Cau 10
+let chuoi = "Tập đoàn Hyosung (Hàn Quốc) dự kiến đầu tư thêm 4 tỷ USD, nâng tổng số vốn rót vào Việt Nam lên gấp đôi.\n" +
+    "Tại cuộc gặp Thủ tướng Phạm Minh Chính chiều 14/10, ông Cho Hyun-joon, Chủ tịch Tập đoàn Hyosung (Hàn Quốc), khẳng định môi trường đầu tư của Việt Nam rất đáng tin cậy. Ông tin rằng Việt Nam sẽ trở thành trung tâm sản xuất của châu Á.";
+console.log(chuoi.split(" "))
+function findLongestWord(array) {
+    let longestWord = "";
+    array.forEach(function(word) {
+        if(word.length > longestWord.length) {
+            longestWord = word;
+        }
+    });
+    return longestWord;
+}
+let word = findLongestWord(chuoi.split(" "));
+console.log(word);
